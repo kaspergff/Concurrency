6a8073cdef285243014447e13667b9d145c27b55
Stefanvdp <51714394+Stefanvdp@users.noreply.github.com>
2019-12-28 10:39:37 +0100

    palpatine

--- Assignment 2/src/netwerk functies.hs
+++ Assignment 2/src/netwerk functies.hs
@@ -11,20 +11,20 @@
 --upon failure of channel
 --upon repair of channel
 
-Procedure Recompute (v):
-    begin if v = u
-        then begin Du[v] := 0 ; Nbu[v] := local end
-        else begin (* Estimate distance to v *)
-                d := 1 + min{ndisu[w, v] : w ∈ Neighu} ;
-                if d < N then
-                    begin Du[v] := d ;
-                        Nbu[v] := w with 1 + ndisu[w, v] = d
-                    end
-                else begin Du[v] := N ; Nbu[v] := udef end
-            end ;
-        if Du[v] has changed then
-            forall x ∈ Neighu do send h mydist, v, Du[v]i to x
-    end
+-- Procedure Recompute (v):
+--     begin if v = u
+--         then begin Du[v] := 0 ; Nbu[v] := local end
+--         else begin (* Estimate distance to v *)
+--                 d := 1 + min{ndisu[w, v] : w ∈ Neighu} ;
+--                 if d < N then
+--                     begin Du[v] := d ;
+--                         Nbu[v] := w with 1 + ndisu[w, v] = d
+--                     end
+--                 else begin Du[v] := N ; Nbu[v] := udef end
+--             end ;
+--         if Du[v] has changed then
+--             forall x ∈ Neighu do send h mydist, v, Du[v]i to x
+--     end
 
 recompute :: Node -> Int -> STM ()    
 recompute n@(Node {nodeID = me, routingtable = r ,netwerkSize = n, estimatedD = ed}) int = do
@@ -35,12 +35,17 @@ recompute n@(Node {nodeID = me, routingtable = r ,netwerkSize = n, estimatedD = 
         (d, portNumber) = getMinDistanceFromNBto int -- getMinDistanceFromNBto moet vragen aan alle buren of ze de afstand naar de int doorsturen en daar de laagste van kiezen, portnumber = nummer van de buur
         if (d + 1 ) < n
             then = do
-                updataDistanceTable ed int (d+1)
+                updateDistanceTable ed int (d+1)
                 addToRoutingTable r int portNumber (d+1)
         else = do
-            updataDistance ed int n
+            updateDistance ed int n
             addToRoutingTable r int undef -- dit moet sws nog heel anders ff over
     if oldDistance > (d + 1)
         sendmessage (d+1) -- moet die dus naar alle buren zijn afstand naar de int sturen
     
+--getMinDistanceFromNBto :: Int ->  ....
+--updateDistanceTable :: nieuwdatatype... -> ....
+--addToRoutingTable :: Routingtable -> nieuwdatatype... -> Int -> Int
+--updateDistance :: nieuwdatatype -> Int -> Int
+--sendmessage :: Int -> Case??? -> StM()/IO()
 
